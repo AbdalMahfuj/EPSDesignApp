f01f8d67c5d8fa224abccf02db986fe40bed0a39
Mahfuj <abda>
2023-03-07 17:59:49 +0600

    tableview cell designing..

--- EPSDesignApp/BillTableViewCell.swift
+++ EPSDesignApp/BillTableViewCell.swift
@@ -35,12 +35,12 @@ class BillTableViewCell: UITableViewCell {
 
 extension BillTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = billCollectionView.dequeueReusableCell(withReuseIdentifier: "UtilityCollectionViewCell", for: indexPath) as! UtilityCollectionViewCell
-        cell.setUI(utility: "Gas Bill")
+        cell.setUI(utility: "Gas")
         return cell
     }
     
